c13f6cd1dc26759527bbced57c02ba58e63eb9c0
Stinium_Ruide <lohjiongrui@u.nus.edu>
2023-02-09 20:44:22 +0800

    Update 9.2

--- getInputs.py
+++ getInputs.py
@@ -1,6 +1,9 @@
-language = input("Pick language: ")
+language = input("What language do you prefer: ")
+name = input("Name:  ")
+gender = input("Gender: ")
+location = input("Address: ")
+independence = input("Are you able to work independently (Y/N): ")
+education = input("Highest form of education you hold: ")
 
-name = input("Name: ")
-
-
+print("Thank you for the information. Now, please tell us about yourself and we will generate a resume for you shortly.")
 
